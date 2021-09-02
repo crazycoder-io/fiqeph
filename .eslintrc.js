1e9d895064c696f6cc966505d10363895408f8e1
Mesut <kilincaslanmesut@gmail.com>
2021-09-02 15:50:19 +0300

    Create app & implement eslint,pretter

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+        jest: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
+    plugins: ["react", "react-hooks", "prettier", "@typescript-eslint"],
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "off"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "none",
+  tabWidth: 4,
+  arrowParens: "avoid",
+  printWidth: 100,
+};
