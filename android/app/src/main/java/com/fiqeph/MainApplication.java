52a0115e54cf4cd31e7edfe0a92abffd171e7e59
Mesut <kilincaslanmesut@gmail.com>
2021-09-04 01:32:37 +0300

    Implement redux and saga

--- android/app/src/main/java/com/fiqeph/MainApplication.java
+++ android/app/src/main/java/com/fiqeph/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
--- index.js
+++ index.js
@@ -5,11 +5,16 @@ import App from "./src/App";
 import {name as appName} from "./app.json";
 import {Provider as PaperProvider} from "react-native-paper";
 import "react-native-gesture-handler";
+import {Provider} from "react-redux";
+
+import store from "./src/store";
 
 export default function Main() {
     return (
         <PaperProvider>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </PaperProvider>
     );
 }
