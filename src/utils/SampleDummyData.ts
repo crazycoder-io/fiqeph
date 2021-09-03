ec11afd560192ef73087e835ada0b435e401d94e
Mesut <kilincaslanmesut@gmail.com>
2021-09-04 01:35:16 +0300

    Update dummy data

--- src/utils/SampleDummyData.ts
+++ src/utils/SampleDummyData.ts
@@ -1,36 +1,36 @@
 export const PHOTOS = [
     {
-        uri: "https://picsum.photos/1000",
+        uri: "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MXwxfGFsbHwxfHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title1",
         id: 1
     },
     {
-        uri: "https://picsum.photos/800",
+        uri: "https://images.unsplash.com/photo-1630371251263-eb7796cd4118?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MHwxfGFsbHwyfHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title2",
         id: 2
     },
     {
-        uri: "https://picsum.photos/900",
+        uri: "https://images.unsplash.com/photo-1630371251489-06a1fd92dd9c?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MHwxfGFsbHwzfHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title3",
         id: 3
     },
     {
-        uri: "https://picsum.photos/400",
+        uri: "https://images.unsplash.com/photo-1630636755810-0d1a9b556512?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MHwxfGFsbHw0fHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title4",
         id: 4
     },
     {
-        uri: "https://picsum.photos/500",
+        uri: "https://images.unsplash.com/photo-1630630910992-46f7656ab211?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MHwxfGFsbHw1fHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title5",
         id: 5
     },
     {
-        uri: "https://picsum.photos/600",
+        uri: "https://images.unsplash.com/photo-1606787620651-3f8e15e00662?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MXwxfGFsbHw2fHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title6",
         id: 6
     },
     {
-        uri: "https://picsum.photos/700",
+        uri: "https://images.unsplash.com/photo-1630649688295-798a4be9e1dc?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTgyMzh8MHwxfGFsbHw3fHx8fHx8Mnx8MTYzMDY5NDY1Ng&ixlib=rb-1.2.1&q=85",
         title: "Title7",
         id: 7
     }
