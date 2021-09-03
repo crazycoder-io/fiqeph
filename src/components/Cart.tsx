29ccd4548a128bf1dcdb187beab59feac4c62510
Mesut <kilincaslanmesut@gmail.com>
2021-09-03 15:23:07 +0300

    Create card component with dummy data

---
+++ src/components/Cart.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {Card, IconButton, Colors} from "react-native-paper";
+import {CardComponentProps} from "../types";
+
+const Cart: React.FC<CardComponentProps> = (props): JSX.Element => {
+    const {cardData} = props;
+
+    return (
+        <>
+            {cardData.length > 0 ? (
+                cardData.map(card => (
+                    <Card key={card.id}>
+                        <Card.Cover source={{uri: card.uri}} />
+                        <Card.Actions>
+                            <IconButton
+                                size={20}
+                                icon="share"
+                                color={Colors.blue400}
+                                onPress={() => console.log("Pressed")}
+                            />
+                            <IconButton
+                                size={20}
+                                icon="download"
+                                color={Colors.red500}
+                                onPress={() => console.log("Pressed")}
+                            />
+                        </Card.Actions>
+                    </Card>
+                ))
+            ) : (
+                <Card>
+                    <Card.Cover source={{uri: "https://picsum.photos/600"}} />
+                    <Card.Actions>
+                        <IconButton
+                            size={20}
+                            icon="share"
+                            color={Colors.blue400}
+                            onPress={() => console.log("Pressed")}
+                        />
+                        <IconButton
+                            size={20}
+                            icon="download"
+                            color={Colors.red500}
+                            onPress={() => console.log("Pressed")}
+                        />
+                    </Card.Actions>
+                </Card>
+            )}
+        </>
+    );
+};
+
+export default Cart;
---
+++ src/utils/SampleDummyData.ts
@@ -0,0 +1,37 @@
+export const PHOTOS = [
+    {
+        uri: "https://picsum.photos/1000",
+        title: "Title1",
+        id: 1
+    },
+    {
+        uri: "https://picsum.photos/800",
+        title: "Title2",
+        id: 2
+    },
+    {
+        uri: "https://picsum.photos/900",
+        title: "Title3",
+        id: 3
+    },
+    {
+        uri: "https://picsum.photos/400",
+        title: "Title4",
+        id: 4
+    },
+    {
+        uri: "https://picsum.photos/500",
+        title: "Title5",
+        id: 5
+    },
+    {
+        uri: "https://picsum.photos/600",
+        title: "Title6",
+        id: 6
+    },
+    {
+        uri: "https://picsum.photos/700",
+        title: "Title7",
+        id: 7
+    }
+];
