1763890547cfeb6ee834881b44f0b3ffe89333bf
Mesut <kilincaslanmesut@gmail.com>
2021-09-04 12:58:55 +0300

    Update prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,8 +3,8 @@ module.exports = {
     jsxBracketSameLine: true,
     singleQuote: false,
     semi: true,
-    trailingComma: "none",
+    trailingComma: "all",
     tabWidth: 4,
     arrowParens: "avoid",
-    printWidth: 100
+    printWidth: 100,
 };
