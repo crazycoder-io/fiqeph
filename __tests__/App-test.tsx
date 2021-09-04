d4b2f41e08b8f31381851744d07024d3717239b5
Mesut <kilincaslanmesut@gmail.com>
2021-09-04 13:55:48 +0300

    Update test file

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,8 @@ import App from "../src/App";
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
-it("renders correctly", () => {
-    renderer.create(<App />);
+describe("Test", () => {
+    test("renders correctly", () => {
+        renderer.create(<App />);
+    });
 });
